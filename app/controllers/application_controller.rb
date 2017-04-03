a3a3723f2809acd0bb3d9611768c49d57522ffe4
makun <michaelmakun@aliyun.com>
2017-04-03 18:11:58 +0800

    implement cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,32 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+
+  helper_method :current_cart
+
+  def current_cart
+    @current_cart ||= find_cart
+  end
+
+  private
+
+  def find_cart
+    if !current_user
+      cart = Cart.find_by(id: session[:cart_id])
+      if cart.blank?
+        cart = Cart.create
+      end
+      session[:cart_id] = cart.id
+      return cart
+    else
+      cart = Cart.find_by(id: session[:user_id])
+      if cart.blank?
+        cart = Cart.create
+      end
+      session[:user_id] = current_user.id
+      return cart
+    end
+  end
+
+
 end
