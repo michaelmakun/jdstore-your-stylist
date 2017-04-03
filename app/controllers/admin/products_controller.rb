78d417984b981ee3d6ff5f39e06413af21fa2669
makun <michaelmakun@aliyun.com>
2017-04-03 12:26:44 +0800

    add admin to user

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :require_admin
 
   def index
     @products = Product.all
